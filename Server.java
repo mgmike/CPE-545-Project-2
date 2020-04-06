cb367a86b6fb817ebc75c0cab7e5dbe2ec10d1be
mgmike1023 <mgmike1023@aol.com>
2020-04-05 22:07:59 -0400

    Replaced ArrayList of strings with Workout objects

--- Server.java
+++ Server.java
@@ -4,54 +4,98 @@ import java.net.*;
 import java.util.*;
 
 public class Server extends java.rmi.server.UnicastRemoteObject implements QueryInterface{
-    String addr;
-    Registry registry;
-    ArrayList<String> workoutList = new ArrayList<>(Arrays.asList("Bench Press Chest", "Squat Legs", "Arnold Press Shoulders", 
-                                                                "Push Press Shoulders", "Overhead Press Shoulders", 
-                                                                "Military Press Shoulders", "Upright Row Shoulders", 
-                                                                "Pull-ups Back", "Dips Triceps", "Lateral Pull-down Back", 
-                                                                "Seated Row Back", "Dumbbell Row Back", "Row Back", "Curls Bicep", 
-                                                                "Barbell Row Back", "Preacher Curls Biceps", 
-                                                                "Incline Bench Curls Biceps", "Incline Bench Press Chest", 
-                                                                "Decline Bench Press Chest", "Flies Chest", "Skull-crusher Triceps", 
-                                                                "Close-grip Bench Triceps", "Overhead Extension Triceps"));
+    private String addr;
+    private Registry registry;
+    private static ArrayList<Workout> workoutList = new ArrayList<Workout>();
 
     @Override
-    public String queryWorkout(String x) throws RemoteException{
-	ArrayList<String> workoutFound = new ArrayList<String>();
-	for (int i = 0; i < workoutList.size(); i++) {
-            if (x.equals(workoutList.get(i).split("\\s+")[0])){
-		workoutFound.add(workoutList.get(i).split("\\s+")[1]);
+    public String queryWorkout(String muscleGroup) throws RemoteException{
+        ArrayList<Workout> muscleGroupFound = new ArrayList<Workout>();
+        for (int i = 0; i<workoutList.size(); i++){
+            if(workoutList.get(i).getMuscleGroup().equals(muscleGroup)){
+                muscleGroupFound.add(workoutList.get(i));
             }
-	}
-	if (workoutFound.size() < 1)
-            return "This exercise is not in the list of known workouts!";
-	else
-            return Arrays.toString(workoutFound.toArray());
+        }
+
+        if(muscleGroupFound.size() < 1){
+                System.out.println("Message recieved");
+                return "This name is not in the class roster";
+        } else{
+                return Arrays.toString(muscleGroupFound.toArray());
+        }
     }
 
     public Server() throws RemoteException{
-	try{
-            addr = (InetAddress.getLocalHost()).toString();
-	} catch(Exception e){
-            System.out.println("Cant get inet address!");
+        try{
+                addr = (InetAddress.getLocalHost()).toString();
+        } catch(Exception e){
+                System.out.println("cant get inet address");
+            }
+        int port = 3232;
+        System.out.println("this address= " + addr + ", port= " + port);
+
+        try{
+                registry = LocateRegistry.createRegistry(port);
+                registry.rebind("Server", this);
+        } catch(RemoteException e){
+                System.out.println("remote exception " + e);
         }
-	int port = 3232;
-	System.out.println("This address: " + addr + ", port: " + port);
-	try{
-            registry = LocateRegistry.createRegistry(port);
-            registry.rebind("Server", this);
-	} catch(RemoteException e){
-            System.out.println("Remote exception: " + e);
-	}
+    }
+
+    private static void populateWorkoutList(){
+        workoutList.add(new Workout("Bench Press", "Chest"));
+        workoutList.add(new Workout("Squat", "Legs"));
+        workoutList.add(new Workout("Arnold Press", "Shoulders"));
+        workoutList.add(new Workout("Push Press", "Shoulders"));
+        workoutList.add(new Workout("Overhead Press", "Shoulders"));
+        workoutList.add(new Workout("Military Press", "Shoulders"));
+        workoutList.add(new Workout("Upright Row", "Shoulders"));
+        workoutList.add(new Workout("Pull-ups", "Back"));
+        workoutList.add(new Workout("Dips", "Triceps"));
+        workoutList.add(new Workout("Lateral Pull-down", "Back"));
+        workoutList.add(new Workout("Seated Row", "Back"));
+        workoutList.add(new Workout("Dumbbell Row", "Back"));
+        workoutList.add(new Workout("Row", "Back"));
+        workoutList.add(new Workout("Curls", "Bicep"));
+        workoutList.add(new Workout("Overhead Extension", "Triceps"));
+        workoutList.add(new Workout("Close-grip Bench", "Triceps"));
     }
 
     public static void main(String args[]){
-	try{
-            Server server = new Server();
-	} catch (Exception e){
-            e.printStackTrace();
-            System.exit(1);
-	}
+        populateWorkoutList();
+        try{
+                Server server = new Server();
+
+        } catch (Exception e){
+                e.printStackTrace();
+                System.exit(1);
+        }
     }
+}
+
+class Workout{
+    private String name;
+    private String muscleGroup;
+
+    public Workout(String n, String mg){
+        name = n;
+        muscleGroup = mg;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMuscleGroup(String muscleGroup) {
+        this.muscleGroup = muscleGroup;
+    }
+
+    public String getMuscleGroup() {
+        return muscleGroup;
+    }
+
+    public String getName(){
+        return name;
+    }
+
 }
